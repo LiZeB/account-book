3715ab8aa2da8780616c856f020fdce25c37c1b2
zhangyaxian <zhangyaxian@hikvision.com>
2021-10-11 20:59:30 +0800

    [add]

--- account-node/model/original-data.js
+++ account-node/model/original-data.js
@@ -1,21 +1,25 @@
 const mongoose = require('mongoose')
+// 定义模式
 const Schema = mongoose.Schema
 const moment = require('moment')
 
 const OriginalDataSchema = new Schema({
-  payType: { type: Integer },   // 开销类型
-  payName: { type: String },    // 开销名称
-  isSpecial: { type: Boolean },  // 是否为特殊项，false：否，true：是
-  payValue: { type: Double },    // 开销金额
-  user: { type: String },       // 开销人
-  _consumeTime: { type: TimeStamp },  // 开销日期
-  remark: { type: String },    // 备注
+  consumeType: { type: Number },  // 开销类型
+  consumeName: { type: String },  // 开销名称
+  consumeSum: { type: Number },  // 开销金额
+  _consumeTime: { type: Date },  // 开销日期
+  consumer: { type: String },  // 开销人
+  isSpecial: { type: Boolean },  // 是否为特殊项
+  remark: { type: String },  // 备注
 });
 
+// 虚拟属性
 OriginalDataSchema.virtual("consumeTime").get(function () {
   return moment(this._consumeTime).format("YYYY-MM-DD HH:mm:SS");
 });
 
+// 编译模型
 const OriginalData = mongoose.model('OriginalData', OriginalDataSchema)
 
 module.exports = OriginalData
+
--- account-node/src/app.js
+++ account-node/src/app.js
@@ -1,14 +1,48 @@
 const mongoose = require('mongoose')
-const dbUrl = "mongodb://localhost:27017/todo_development"
-mongoose.connect(dbUrl)
+const MongoClient = require('mongodb').MongoClient
+
+// 使用全局Promise库
 mongoose.Promise = global.Promise
+
+// 默认数据库
+const dbName = "test"
+const serverUrl = "mongodb://localhost:27017/"
+const dbUrl = serverUrl + dbName;
+
+// OriginalData
 const OriginalData = require('../model/original-data.js')
+
+// 连接服务器
+const client = new MongoClient(serverUrl)
+client.connect(function (err) {
+  if (err) throw err
+  console.log("成功连接到MongoDB服务器！")
+  const db = client.db(dbName);
+  const collection = db.collection('OriginalData');
+  // 插入记录
+  // db.originalData.insert({"consumeType":0,"consumeName":"","consumeSum":0,"_consumeTime":"2021 - 10 - 11T11: 00: 23.000 + 00: 00","consumer":"","isSpecial":false,"remark":""})
+  collection.insertOne({
+    "consumeType": 0, 
+    "consumeName": "", 
+    "consumeSum": 0, 
+    "_consumeTime": "2021 - 10 - 11T11: 00: 23.000 + 00: 00", 
+    "consumer": "", 
+    "isSpecial": false, 
+    "remark": ""
+  }, function (err) {
+    if (err) throw err
+  })
+  client.close();
+})
+
+// mongoose.connect(dbUrl)
+// const connection = mongoose.connection
+// connection.on("error", console.error.bind(console, "数据库连接错误！"))
 
 const express = require('express');
 const app = express();
 app.use(express.json()); //express.json 解析 JSON 格式的请求体数据（有兼容性，仅在 4.16.0+ 版本中可用）
 app.use(express.urlencoded());
-
 
 app.use((err, req, res, next) => {
   if (err) {
