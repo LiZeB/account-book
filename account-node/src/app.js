e9fca68fd8c86f30ddd14f8690cdcd5aeacc8771
lizebin <963899158@qq.com>
2023-04-05 00:56:35 +0800

    [feat]完成了1.0.0的记账本应用开发

--- account-node/src/app.js
+++ account-node/src/app.js
@@ -3,15 +3,14 @@ const MongoClient = require('mongodb').MongoClient;
 const path = require('path');
 const history = require('connect-history-api-fallback');
 
+/************ 1.Mongoose连接数据库 *********/
 mongoose.Promise = global.Promise
 const env = process.env.NODE_ENV || 'development'
-let db, dbUrl = "";   // TODO: dbUrl是数据库地址，打包时需填写真实地址
+let db, dbUrl = "";   // NOTE: dbUrl是数据库地址，如果数据库和业务组件不一致时需要填写
 if (env === 'development') {
   dbUrl = "mongodb://localhost:27017/";
 }
-
 const dbName = "account-book";
-// MongoClient操作一个数据库或多个数据库
 const client = new MongoClient(dbUrl);
 client.connect(function (err) {
   if (err) throw err
@@ -19,17 +18,15 @@ client.connect(function (err) {
   db = client.db(dbName);
   client.close();
 });
-
-// Mongoose连接数据库
 mongoose.connect(dbUrl+dbName);
 const connection = mongoose.connection;
 connection.on("error", console.error.bind(console, "数据库连接错误！"));
 
+/************* 2.创建express实例、分发路由、设置静态文件代理 ******/
 const express = require('express');
 const app = express();
-app.use(express.json()); // express.json 解析 JSON 格式的请求体数据（有兼容性，仅在 4.16.0+ 版本中可用）
+app.use(express.json());
 app.use(express.urlencoded());
-
 const baseRoutes = require('../api/base');
 app.use('/account-web/base', baseRoutes);
 const dicRoutes = require("../api/dic")
@@ -38,8 +35,7 @@ const uploadRoutes = require("../api/upload")
 app.use('/account-web/upload', uploadRoutes);
 const statisticRoutes = require("../api/statistic");
 app.use('/account-web/statistic', statisticRoutes);
-
-app.use(history());
+app.use(history()); // 解决由于history模式下前端路由存在导致后台无法匹配到某个text/html,返回404的问题。不匹配时，强行指向 index.html
 app.use(express.static(path.join(__dirname, '../dist')));
 
 app.use((err, req, res, next) => {
